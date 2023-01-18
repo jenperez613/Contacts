30c8f7a2cbcee8f4f5786087557be7656303173a
Jen Perez <jenperez613@icloud.com>
2023-01-18 16:15:47 -0500

    added prop-types

---
+++ src/components/ListContacts.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ListContacts = ({ contacts, onDeleteContact }) => {
+
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => (
+        <li key={contact.id} className="contact-list-item">
+          <div className="contact-avatar" style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }} />
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button onClick={() => onDeleteContact(contact)} className="contact-remove">
+            Remove
+          </button>
+        </li>
+      ))}
+      </ol>
+  )
+}
+
+export default ListContacts
